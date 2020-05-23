e7ccc5dde37aec6f7e8856ffa209475d976c29a1
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-05-23 17:43:59 +0530

    Update js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function endGame(draw) {
     if (draw) {
     winningMessageTextElement.innerText = 'Draw!'
     } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} WINS!`
+        winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} WINS!`
     }
     winningMessageElement.classList.add('show')
 }
